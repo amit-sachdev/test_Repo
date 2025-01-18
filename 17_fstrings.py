1ff50f87048cc3cccbac4474a35e737295d6168c
Amit Ji <amitsachdev2911@gmail.com>
2025-01-18 18:12:10 +0530

    Added a feature

---
+++ 17_fstrings.py
@@ -0,0 +1,11 @@
+name="Amit"
+surname="Sachdev"
+country="India"
+print(f"my name is {name} {surname} from {country} ")
+# o/p --> my name is Amit Sachdev from India
+price=89.99999
+txt=f"price of gold for 0.001 gm is {price:.2f}"  #.2f will round off till second decimal placce
+print(txt) 
+
+#  curly bracket 2 kar lena jab string vo daal rhe ho jisme curly bracket hai example name ="{amit}"
+#  in fstring write it as --> f"my name is {{amit}}"
